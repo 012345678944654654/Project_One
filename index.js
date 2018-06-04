1a5c620afa8d4c08aa9380f1efcf21b62eb503a5
Rajković Milan <33525833+012345678944654654@users.noreply.github.com>
2018-06-04 16:47:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-document.getElementById("myButton").onclick = function () {
-        location.href = "Restoran.html";
-    };
-
-document.getElementById("myButton_ponude").onclick = function () {
-        location.href = "Specijalne ponude.html";
-    };
-
-document.getElementById("myButton_sadrzaj").onclick = function () {
-        location.href = "Sadrzaj.html";
-    };
+document.getElementById("myButton").onclick = function () {location.href = "Restoran.html";};
+document.getElementById("myButton_ponude").onclick = function (){location.href = "Specijalne ponude.html";};
+document.getElementById("myButton_sadrzaj").onclick = function () {location.href = "Sadrzaj.html";};
